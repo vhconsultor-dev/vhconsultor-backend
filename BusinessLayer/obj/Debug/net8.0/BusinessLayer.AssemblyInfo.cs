789a4918b728e867f88d914336a2dfe8fdbd0259
jamartinezu <jamartinezu@grupopurdy.com>
2025-10-09 19:15:28 -0600

    dd
    dd

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be91a63ebb95b6447efe3e111188cd232e3cfe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8ea514238356a484606f1a3db3f5725e36b887")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
