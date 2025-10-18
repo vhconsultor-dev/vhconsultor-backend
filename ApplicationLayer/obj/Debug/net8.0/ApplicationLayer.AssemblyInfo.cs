d47eaf84d3eff9ae0bcc56076c6f31ed4529d784
jamartinezu <jamartinezu@grupopurdy.com>
2025-10-17 20:36:41 -0600

    1.0.3

--- ApplicationLayer/obj/Debug/net8.0/ApplicationLayer.AssemblyInfo.cs
+++ ApplicationLayer/obj/Debug/net8.0/ApplicationLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1a09c39beff03595498b55f08952f98fb59e9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8cc8a52c8236f582c7e8649ddd4f27b6d8a003")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
